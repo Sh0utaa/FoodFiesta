f3ffbb4e8f98c9c570d4b41af06fa186c7a61604
ShotaTevdorasvhili <stevdorashvili08@gmail.com>
2023-12-07 22:12:11 +0400

    Added javascript to frontend + pictures

--- FoodFiestaWebsite/Controllers/HomeController.cs
+++ FoodFiestaWebsite/Controllers/HomeController.cs
@@ -14,7 +14,6 @@ namespace FoodFiestaWebsite.Controllers
             _logger = logger;
         }
 
-        [Authorize]
         public IActionResult Index()
         {
             return View();
---
+++ FoodFiestaWebsite/Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodFiestaWebsite.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Food()
+        {
+            return View();
+        }
+        public IActionResult Drinks()
+        {
+            return View();
+        }
+    }
+}
